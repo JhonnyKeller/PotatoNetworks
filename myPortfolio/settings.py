4a1da60214c6983ab5b2f421ec72c6add3ccefc8
Jhonny <jhonnykellerdev@gmail.com>
2022-10-26 16:05:11 +0100

    'addmediaroot'

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -205,8 +205,8 @@ if DEBUG:
     ]
 else:
     STATIC_URL = '/static/'
-    MEDIA_ROOT = os.path.join(MEDIA_DIR)
-    STATIC_ROOT = os.path.join(STATIC_DIR)
+    MEDIA_ROOT = os.path.join(BASE_DIR , 'media/')
+    STATIC_ROOT = os.path.join(BASE_DIR , 'static/')
 
 
 # Default primary key field type
