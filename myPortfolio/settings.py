bfd65a43d18612cac79857e659133a4bf0890262
Jhonny <jhonnykellerdev@gmail.com>
2022-10-26 18:11:45 +0100

    'okfine'

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -188,11 +188,8 @@ PASSWORD_RESET_TIMEOUT = 14400
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
-STATIC_ROOT = os.path.join(BASE_DIR , 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
